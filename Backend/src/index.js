df0fe41706a6364d22bbefcd3f0e6e4e2cca69eb
mubarak7785 <mubarakjinabade07@gmail.com>
2023-04-09 15:36:57 +0530

    made mvc

---
+++ Backend/src/index.js
@@ -0,0 +1,23 @@
+const express=require("express")
+const cors = require("cors")
+
+const app = express ()
+app.use(cors())
+const port=process.env.PORT || 2089
+app.use(express.json())
+
+const connect=require("./configs/db")
+
+const registerController=require("./controllers/register.controller")
+app.use("/register",registerController)
+
+
+app.listen(port,async function (req,res){
+    try{
+        await connect();
+        console.log ("Listening Port 2124")
+    }catch(err){
+        console.log(err.message)
+    }
+})
+
